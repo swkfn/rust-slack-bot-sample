952783efb369c3a4dd3c4b3a89cadbb68bcf4d51
swkfn <sw.kfn4410@gmail.com>
2019-06-24 18:56:27 +0900

    api取得のとこまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// extern crate rust-slack-bot-sample;
+extern crate slack;
+
+//use rust_slack_bot::handler;
+//use slack::RtmClient;
+use std::{fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let api_key: String = api_key();
+    println!("{}", api_key);
+    //let mut handler = handler::Handler;
+    //let r = RtmClient::login_and_run(&api_key, &mut handler);
+    /*
+    match r {
+        Ok(_) => {},
+        Err(err) => panic!("Error: {}", err)
+    }
+    */
+}
+
+fn api_key() ->String {
+    let content = fs::read_to_string("slack_api");
+    match content {
+        Ok(content) => content,
+        Err(_) => {
+            println!("Required the SLACK_API_TOKEN environment variable");
+            process::exit(1);
+        }
+    }
 }
