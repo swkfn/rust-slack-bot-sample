3d6af2ec2a105433a2567c92d57d58908cc5a9d6
swkfn <sw.kfn4410@gmail.com>
2019-06-24 19:25:34 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-// extern crate rust-slack-bot-sample;
+extern crate rust_slack_bot_sample;
 extern crate slack;
-
-//use rust_slack_bot::handler;
-//use slack::RtmClient;
+/*
+use rust_slack_bot_sample::handler;
+use slack::RtmClient;
+*/
 use std::{fs, process};
 
 fn main() {
     let api_key: String = api_key();
     println!("{}", api_key);
-    //let mut handler = handler::Handler;
-    //let r = RtmClient::login_and_run(&api_key, &mut handler);
     /*
+    let mut handler = handler::Handler;
+    let r = RtmClient::login_and_run(&api_key, &mut handler);
     match r {
         Ok(_) => {},
         Err(err) => panic!("Error: {}", err)
@@ -18,7 +19,7 @@ fn main() {
     */
 }
 
-fn api_key() ->String {
+fn api_key() -> String {
     let content = fs::read_to_string("slack_api");
     match content {
         Ok(content) => content,
